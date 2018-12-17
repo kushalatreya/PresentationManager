d1996e9a8cb5de7b9e011af0a7eb798efd8cd0e4
kushal atreya <kushalatreya@live.com>
2018-12-17 13:44:33 +0200

    adding delete functionality

--- src/components/EachPresentation.js
+++ src/components/EachPresentation.js
@@ -1,20 +1,27 @@
-import React from 'react'
-import './eachPost.css'
+import React, { Component } from "react";
+import "./eachPost.css";
 
-export default function EachPresentation(props) {
-  return (
-    <div>
-     { props.post.posts.map((element,index)=>{
-          return<div className='each_post' key={index}>
+export default class EachPresentation extends Component {
+  
+  render() {
+    return (
+      <div>
+        {this.props.post.posts.map((element, index) => {
+          return (
+            <div className="each_post" key={element.id}>
               <p>{element.id}</p>
               <p>{element.presenter_name}</p>
               <p>{element.evaluator_name}</p>
               <p>{element.presentaion_topic}</p>
               <p>{element.presentation_URL}</p>
               <button>Edit</button>
-              <button>Delete</button>
+              <button onClick={this.props.deleteclicked(this.props.id)}>
+                Delete
+              </button>
             </div>
-      })}
-    </div>
-  )
+          );
+        })}
+      </div>
+    );
+  }
 }
